682afee13fb7663c5e7b6849324c3bd7c079155b
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-06-18 17:20:55 -0300

    feat: routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,9 @@ import SnackController from './controller/SnackController';
 
 const routes = Router();
 
-routes.get('/snacks', SnackController.find)
-routes.get('/snack/:id', SnackController.findById)
+routes.get('/snacks', SnackController.find);
+routes.get('/snack/:id', SnackController.findById);
 routes.post('/snack', SnackController.create);
+routes.delete('/snack/:id', SnackController.delete);
 
 export default routes;
