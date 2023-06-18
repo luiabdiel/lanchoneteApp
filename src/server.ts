5b4a96631f3c6573ce43bff205d55717134327ac
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-06-17 22:15:26 -0300

    feat: server on

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import express  from 'express';
 
 const app = express();
 
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hi, Node.js')
+});
+
 app.listen(3000, () => {
-  console.log('🔥 Server started at http://localhost:3000')
+  console.log('🔥 Server started at http://localhost:3000');
 })
