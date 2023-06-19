4bb2297ca22f4852a95f8a09e8f00bc5c57c0b26
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-06-19 09:28:14 -0300

    refactor: server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,27 @@
+import http from 'node:http';
 import express  from 'express';
 import mongoose from 'mongoose';
 import routes from './routes';
 
 const app = express();
+const server = http.createServer(app);
 
-mongoose.connect('mongodb://localhost/snackapi');
+mongoose.connect('mongodb://localhost/snackapi')
+  .then(() => {
+    const port = 3001;
 
-app.use(express.json());
-app.use(routes);
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
 
-app.listen(3000, () => {
-  console.log('🔥 Server started at http://localhost:3000');
-});
+      next();
+    });
+    app.use(express.json());
+    app.use(routes);
+
+    server.listen(port, () => {
+      console.log(`🔥 Server started at http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('Erro ao conectar no mongodb'));
