d167edc489735d7e108609ab31bbe896d53c7441
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-06-18 22:47:42 -0300

    refactor: route name

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import SnackController from './controller/SnackController';
 
 const routes = Router();
 
-routes.get('/snacks', SnackController.find);
+routes.get('/snack', SnackController.find);
 routes.get('/snack/:id', SnackController.findById);
 routes.post('/snack', SnackController.create);
 routes.put('/snack/:id', SnackController.change);
